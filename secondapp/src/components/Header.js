005d0ecd9eb6d4ad8c53a95093ded08789d8f179
NIKHIL SHRESTHA <shrestha.nikhil0025@gmail.com>
2024-04-13 15:17:04 +0530

    complete header section.

--- secondapp/src/components/Header.js
+++ secondapp/src/components/Header.js
@@ -1,12 +1,21 @@
+import React from 'react';
+import bannerImg from '../images/restauranfood.jpg'
+
 function Header(){
     return(
         <header className='header'>
             <section>
                 {/* banner text */}
-                <div>
+                <div className='banner'>
                     <h2>Little Lemon</h2>
                     <h3>Chicago</h3>
                     <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+                    <button aria-label='On Click'>Reserve Table</button>
+                </div>
+
+                {/* banner image */}
+                <div className='banner-img'>
+                    <img src={bannerImg} alt=''/>
                 </div>
             </section>
         </header>
